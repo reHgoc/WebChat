50507a0f71a71f044546e7db433f8d952048bd15
reHgoc <spark1993g@mail.ru>
2022-04-25 13:21:50 +0200

    Make TODO list; make adding elements to list

--- JS/test.js
+++ JS/test.js
@@ -1,7 +1,25 @@
-let btn = document.getElementById("btn");
+let ListTxt = document.querySelector("ul");
+let button = document.getElementById("AddBtn");
+let li = null;
 
-btn.onclick = myFunction;
+button.onclick = AddElement;
+li.onclick = RemoveElement;
 
-function myFunction() {
- alert("Hi");
+function AddElement(){
+  let elementForAdd = document.createElement("li");
+  let inputValue = document.getElementById("AddTxt").value;
+  let text = document.createTextNode(inputValue);
+  elementForAdd.appendChild(text);
+
+ inputValue == "" ? alert("Field is empty!") : document.getElementById("ListTxt").appendChild(elementForAdd);
+ document.getElementById("AddTxt").value = "";
+ for(let i; i < ListTxt.childNodes.length; i++){
+   if(ListTxt.childNodes[i].tagName == "li"){
+     li = ListTxt.childNodes[i];
+   }
+ }
+}
+//не получается сделать ремув
+function RemoveElement(){
+  alert(li);
 }
